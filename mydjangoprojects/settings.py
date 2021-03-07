3491d07f797bcf877fc2524917fe4cb3474a35f8
github username <github email address>
2021-03-07 09:26:56 -0500

    first

--- mydjangoprojects/settings.py
+++ mydjangoprojects/settings.py
@@ -141,9 +141,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Add these new lines
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # django_heroku.settings(locals())
