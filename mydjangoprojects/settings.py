2f49c76724784d8d5911d9014d6c1cbb30ba18a6
github username <github email address>
2021-03-08 14:37:01 -0500

    new domain

--- mydjangoprojects/settings.py
+++ mydjangoprojects/settings.py
@@ -43,8 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'urlshortener',
-   
-    'account'
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
